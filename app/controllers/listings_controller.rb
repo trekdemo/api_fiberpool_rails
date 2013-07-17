77071a6af63a5040f00cf9862bfde73edff5e6a3
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-07-17 12:49:37 +0200

    Increase db connection number

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
 
   def index
+    # Listing.find_by_sql('select pg_sleep(1)')
+    # EM::Synchrony.sleep(1)
     render json: Listing.limit(50)
   end
 
